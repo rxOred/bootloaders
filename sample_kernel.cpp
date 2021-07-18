1a162808f7e8796b1f66ff71e40cd3ba8f12c4ef
Jayod Bandara <rxored@gmail.com>
2021-07-18 06:53:05 +0000

    Update sample_kernel.cpp

--- sample_kernel.cpp
+++ sample_kernel.cpp
@@ -1,18 +1,1 @@
-#include <stdlib.h>
-#include <stdio.h>
 
-float calculate(double x, double y){
-
-    return x + y;
-}
-
-int calculate(int x, int y){
-
-    return x + y;
-}
-
-int main(void){
-
-    printf("%d\n", calculate(1, 2));
-    printf("%f\n",static_cast<float>(calculate(1, 2)));
-}
